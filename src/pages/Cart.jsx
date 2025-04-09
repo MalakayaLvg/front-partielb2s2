967b2f3f7da0713512d58d92597662d4250fd06e
Oslynx <malakayalvg@gmail.com>
2025-04-09 14:15:34 +0200

    clear cart + validate order

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,62 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    // Initialiser avec un tableau vide plutôt qu'un objet
     const [items, setItems] = useState([]);
+
+    const handleClearCart = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            console.log(token);
+            const response = await axios.post('https://back-partiel.malakayalauvergnat.com/api/cart/clear', {},{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            alert(response)
+            getCart()
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleDeleteItem = async (id) => {
+        try {
+            const token = localStorage.getItem('token');
+            console.log(token);
+            const body = {
+                productId: id
+            }
+            const response = await axios.post('https://back-partiel.malakayalauvergnat.com/api/cart/remove', body,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            alert(response)
+            getCart()
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleValidateOrder = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            console.log(token);
+            const response = await axios.post('https://back-partiel.malakayalauvergnat.com/api/order/validate', {},{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            alert(response.data.message)
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 
     const getCart = async () => {
         try {
@@ -34,10 +86,13 @@ const Cart = () => {
                 {items.length > 0 ? (
                     <ul className="cart-items">
                         {items.map((item) => (
-                            <li key={item.productId} className="cart-item">
+                            <li key={item.productId} className="my-1 form-control d-flex justify-content-between">
                                 <div className="item-details d-flex flex-column">
                                     <span className="item-name">{item.productName} : {item.price} €</span>
                                     <span className="item-quantity">Quantité: {item.quantity}</span>
+                                </div>
+                                <div className="ms-2">
+                                    <button onClick={() => handleDeleteItem(item.productId)} className="btn btn-danger">X</button>
                                 </div>
                             </li>
                         ))}
@@ -46,11 +101,20 @@ const Cart = () => {
                     <p className="empty-cart">Votre panier est vide</p>
                 )}
             </div>
-            <Link to="/" className="mx-1">
-                <button className="btn btn-secondary">
-                    Home
-                </button>
-            </Link>
+            <div className="d-flex flex-column gap-1">
+                <Link to="/" className="mx-1">
+                    <button className="btn btn-secondary">
+                        Home
+                    </button>
+                </Link>
+                <div>
+                    <button onClick={handleClearCart} className="btn btn-danger">Supprimer</button>
+                </div>
+               <div>
+                   <button onClick={handleValidateOrder} className="btn btn-success">Valider</button>
+               </div>
+            </div>
+
         </>
     )
 }
