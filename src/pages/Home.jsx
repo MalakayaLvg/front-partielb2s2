b1d93732d69d737dca7bd10625c456a9b5a7f74f
Oslynx <malakayalvg@gmail.com>
2025-04-09 15:26:23 +0200

    little css fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,27 +6,27 @@ const Home = () => {
     return(
         <>
             <h1>Home page</h1>
-            <Link to="/scan" className="mx-1">
+            <Link to="/scan" className="m-1">
                 <button className="btn btn-secondary">
                     Scan
                 </button>
             </Link>
-            <Link to="/cart" className="mx-1">
+            <Link to="/cart" className="m-1">
                 <button className="btn btn-secondary">
                     Panier
                 </button>
             </Link>
-            <Link to="/login" className="mx-1">
+            <Link to="/login" className="m-1">
                 <button className="btn btn-secondary">
                     Login
                 </button>
             </Link>
-            <Link to="/register" className="mx-1">
+            <Link to="/register" className="m-1">
                 <button className="btn btn-secondary">
                     register
                 </button>
             </Link>
-            <Link to="/order" className="mx-1">
+            <Link to="/order" className="m-1">
                 <button className="btn btn-secondary">
                     Mes commandes
                 </button>
