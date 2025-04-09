391541c849e85e78c9bb5a6b9c3350f08f30c40d
Oslynx <malakayalvg@gmail.com>
2025-04-09 15:12:56 +0200

    navbar fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
